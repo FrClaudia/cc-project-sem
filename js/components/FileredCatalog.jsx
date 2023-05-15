882558490ed128d0dcf1c410ad2956c1e945d580
Claudia Frincu <frincuclaudia19@stud.ase.ro>
2023-05-15 23:18:35 +0300

    new commit

--- js/components/FileredCatalog.jsx
+++ js/components/FileredCatalog.jsx
@@ -1,19 +1,25 @@
-// js/components/MainPage.jsx
+// js/components/FilteredCatalog.jsx
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-export default function MainPage() {
+export default function FilteredCatalog() {
 	const [colors, setColors] = useState([]);
+    const [searchText, setSearchText] = useState('blue');
 
 	const router = useRouter();
 
 	function handleClick() {
-		router.push('/generate');
+		router.push('/');
 	}
 
-	function handleClick2() {
-		router.push('/filteredcatalog');
-	}
+    const handleSearchTextChange = (event) => {
+        setSearchText(event.target.value);
+    };
+
+    const filteredColors = colors.filter((color) =>
+        color.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
 
 	useEffect(() => {
 		try{
@@ -46,6 +52,7 @@ export default function MainPage() {
 		}
 	}
 
+    
 	return (
 		<section className="bg-white dark:bg-gray-900" style = {{background: "linear-gradient(to top, black, #243B55)"}} > 
 			<div className="container px-6 py-10 mx-auto">
@@ -56,24 +63,26 @@ export default function MainPage() {
 				    <button type="button" 
 				           onClick={handleClick}
 				           className="focus:outline-none focus:ring-4 text-black font-medium rounded-lg text-sm px-4 py-2.5 mr-1 mb-1 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 light:bg-gray-800 light:border-gray-700 light:hover:bg-gray-700">
-				        <div style={{ fontWeight: 'bold', fontFamily: 'Georgia' }}>Generate new colors</div>
-				    </button>
-					<button type="button" 
-				           onClick={handleClick2}
-				           className="focus:outline-none focus:ring-4 text-black font-medium rounded-lg text-sm px-4 py-2.5 mr-1 mb-1 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 light:bg-gray-800 light:border-gray-700 light:hover:bg-gray-700">
-				        <div style={{ fontWeight: 'bold', fontFamily: 'Georgia'  }}>Filter list of colors</div>
+				        <div style={{ fontWeight: 'bold', fontFamily: 'Georgia' }}>Back to full list</div>
 				    </button>
 				</div>
 				<p  style={{ fontStyle: 'italic', fontFamily: 'Georgia' }}
-				    className="w-[1000px] mx-auto text-center mt-4 text-3xl ">List of your favorite colors:</p>
-				<div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-					{colors.map(color => (
-						<div
-							key={color._id}
-							style={{background: color.hex_value.toString(), fontFamily: 'Georgia' }}
+				    className="w-[1000px] mx-auto text-center mt-4 text-3xl ">
+                        List of your favorite colors <span style={{ fontWeight: 'bold' }}>filtered</span>:
+                </p>
+                <p style={{marginTop: '20px', fontStyle: 'italic', fontFamily: 'Georgia'}}>Filter colors:</p>
+                <input 
+                    type="text" 
+                    value={searchText} 
+                    onChange={handleSearchTextChange} 
+                    style={{ fontFamily: 'Georgia', padding: '5px', border: "2px solid black", color: 'black' }} 
+                />				
+                <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+                        {filteredColors.map((color) => (
+                            <div key={color._id}
+                            style={{background: color.hex_value.toString(), fontFamily: 'Georgia' }}
 							className="block max-w-sm p-6 rounded-lg">
-
-							<div 
+                             							<div 
 							    className="block max-w-sm p-4 rounded-lg bg-white rounded-lg shadow hover:bg-gray-300 light:bg-gray-800 light:border-gray-700 light:hover:bg-gray-700" >
 								<h5 className="flex justify-center mt-4 mb-2 text-2xl font-bold tracking-tight text-gray-900 light:text-black">
 									{color.name}
@@ -91,18 +100,8 @@ export default function MainPage() {
 									contrast {color.contrast}
 								</div>
 							</div>	
-							<div className={"flex justify-center mt-4"}>
-								<button type="button"
-								        id={color._id}
-								        onClick={deleteColor}
-										style={{ fontWeight: 'bold' }}
-								        className="focus:outline-none focus:ring-4 text-black font-medium rounded-lg text-sm px-4 py-2.5 mr-1 mb-1 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-300 light:bg-gray-800 light:border-gray-700 light:hover:bg-gray-700">
-										Delete color
-											
-								</button>
-							</div>
-						</div>
-					))}
+                            </div>
+                        ))}
 				</div>
 			</div>
 		</section>
---
+++ pages/filteredcatalog.js
@@ -0,0 +1,8 @@
+// pages/generate.js
+import FilteredCatalog from "@/js/components/FileredCatalog";
+
+export default function FileredCatalog() {
+  return (
+      <FilteredCatalog/>
+  )
+}
